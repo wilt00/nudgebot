cd274008d6d897c6bd3bbdd5bc219e164b4a6c7a
Will T <toherwils@gmail.com>
2018-12-18 16:02:18 -0600

    Rip out gcloud

--- src/index.ts
+++ src/index.ts
@@ -1,72 +1,20 @@
-require('@google-cloud/debug-agent').start();
-
 import * as Discord from "discord.js";
 import { inspect } from "util";
-import Datastore = require("@google-cloud/datastore");
+import { env } from "process";
 
-const datastore = new Datastore();
-
-const countK = datastore.key(["TEST", "count"]);
-interface ICount {
-  count: number;
-}
-
-const discordK = datastore.key(["token", "discord"]);
-interface IToken {
-  token: string;
-}
-
-async function initializeCount() {
-  datastore.save({
-    key: countK,
-    data: {
-      count: 0,
-    },
-  }).then(() => {
-    console.log("Count initialization success");
-  }).catch((err) => {
-    console.error("Count initialization failed");
-    console.error(err);
-  });
-}
-
-async function incrementCount() {
-  let err: Error, entity: object = await datastore.get(countK);
-  if (err) {
-    console.error("Update failed on retrieval");
-    console.error(err);
-    return;
-  }
-  let entityCt = entity as ICount[];
-  entityCt[0].count++;
-  datastore.save({
-    key: countK,
-    data: entityCt,
-  }).catch((err) => {
-    console.error("Update failed on writeback");
-    console.error(err);
-  });
-  return entityCt[0].count;
-}
+let count = 0;
 
 const client = new Discord.Client();
 
 client.on("ready", () => {
-  initializeCount();
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", async (msg: Discord.Message) => {
   console.log(`Message: ${inspect(msg)}`);
   if (msg.content === "ping") {
-    msg.reply("shapow!" + await incrementCount());
+    msg.reply(`shapow! ${count}`);
   }
 });
 
-datastore.get(discordK).then((data) => {
-  const dToken = data[0] as IToken;
-  client.login(dToken.token);
-}).catch((err: Error) => {
-  console.error("Failed to retrieve token");
-  console.error(err);
-});
+client.login(env.DISCORD_TOKEN);
